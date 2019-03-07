d95185d85975497de27cca171572ecec0b7eb686
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-03-07 01:48:29 +0100

    update created Struct and method

--- lab1/main.c
+++ lab1/main.c
@@ -4,37 +4,39 @@
 const int SIZE_OF_BUFFER = 512;
 
 struct Searcher {
-    char *folder_name;
-    char *filename;
+    char *dir;
+    char *file;
+    char *name_file_temp;
+};
+
+struct Result {
+    char *result_block;
 };
 
 struct Searcher *searcher;
 
-void initialize_searcher() {
-    searcher = malloc(sizeof(struct Searcher));
+void initialize_searching_array(size_t size_of_array) {
+    searcher = calloc(size_of_array, sizeof(struct Searcher));
 }
 
-void set_searching(char *folder_name, char *filename) {
-    searcher->folder_name = folder_name;
-    searcher->filename = filename;
-}
+void search_directory(char *dir, char *file, char *name_file_temp) {
+    searcher->dir = dir;
+    searcher->file = file;
+    searcher->name_file_temp = name_file_temp;
 
-void call_searching() {
     char buffer[SIZE_OF_BUFFER];
 
     if (snprintf(buffer, sizeof(buffer),
-                 "find %s -name %s > tmp.txt", searcher->folder_name, searcher->filename) >= sizeof(buffer)) {
+                 "find %s -name %s > %s", searcher->dir, searcher->file, searcher->name_file_temp) >= sizeof(buffer)) {
         printf("ERROR");
     } else {
         system(buffer);
     }
 }
 
-int main() {
-    initialize_searcher();
-    set_searching("~/CLionProjects/sysopy/lab1", "main.c");
-    call_searching();
 
-    printf("elo");
+int main() {
+    initialize_searching_array(100);
+    search_directory("~/CLionProjects/sysopy/lab1", "main.c", "tmp.txt");
     return 0;
 }
