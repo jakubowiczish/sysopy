fe88666440f8e4417bd0d85a0a5de2130d75af13
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-01 01:14:43 +0200

    Fix zad1a and add zad2a for cw04

--- cw04/zad1a/main.c
+++ cw04/zad1a/main.c
@@ -3,44 +3,44 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
-int is_running = 1;
+static int is_running = 1;
 
-void ctrl_c_handle(int sg) {
+void ctrl_c_handle_SIGINT(int sg) {
     printf("\nCtrl C pressed - end of program\n");
-    exit(0);
+    exit(sg);
 }
 
-void ctrl_z_handle(int sg) {
-    if (!is_running) {
-        printf("\nCtrl Z pressed - continue with Ctrl Z or terminate with Ctrl C\n");
-    } else {
-        printf("\n");
-    }
+void ctrl_z_handle_SIGTSTP(int sg) {
+    printf("\nCtrl Z pressed - continue with Ctrl Z or terminate with Ctrl C\n");
 
     is_running = 1 - is_running;
 }
 
 int main(int argc, char **argv) {
-    signal(SIGINT, ctrl_c_handle);
+    signal(SIGINT, ctrl_c_handle_SIGINT);
 
-    struct sigaction action;
-    action.sa_handler = ctrl_z_handle;
-    sigemptyset(&action.sa_mask);
-    sigaction(SIGTSTP, &action, NULL);
+    struct sigaction sigaction1;
+//    memset(&sigaction1, 0, sizeof(struct sigaction));
+    sigaction1.sa_handler = ctrl_z_handle_SIGTSTP;
+    sigaction1.sa_flags = 0;
+    sigaction(SIGTSTP, &sigaction1, NULL);
 
     time_t actual_time;
     struct tm *time_info;
 
     printf("\nPRESS CTRL Z TO PAUSE (CONTINUE) OR CTRL C TO TERMINATE\n");
+
     while (1) {
         if (is_running) {
             time(&actual_time);
             time_info = localtime(&actual_time);
 
             printf("Actual time: %s", asctime(time_info));
+            sleep(1);
+        } else {
+            pause();
         }
-
-        sleep(1);
     }
 }
---
+++ cw04/zad1b/print_date.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+while true
+do
+    date
+    sleep 1
+done
