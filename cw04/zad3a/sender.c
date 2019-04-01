6a3fd70b9be75f4dac81c8e74e93c763320a9183
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-01 21:18:47 +0200

    Add cw04/zad3a

---
+++ cw04/zad3a/sender.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "utils.h"
+
+int expected_signal_counter = 0;
+int actual_signal_counter = 0;
+
+void kill_catch(int sig, siginfo_t *siginfo, void *context);
+
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        printf("WRONG NUMBER OF ARGUMENTS! TRY AGAIN!\n");
+        return -1;
+    }
+
+    int pid = atoi(argv[1]);
+    expected_signal_counter = atoi(argv[2]);
+    char *mode = argv[3];
+
+    if (strcmp(mode, "KILL") == 0) {
+        initialize_signals(kill_catch);
+        int i;
+        for (i = 0; i < expected_signal_counter; ++i) {
+            if (kill(pid, SIGUSR1) != 0) {
+                printf("PROBLEM WITH SIGUSR1\n");
+            }
+        }
+
+        if (kill(pid, SIGUSR2) != 0) {
+            printf("PROBLEM WITH SIGUSR2\n");
+        }
+    }
+
+    while (1)  {
+        pause();
+    }
+
+
+}
+
+
+
+void kill_catch(int sig, siginfo_t *siginfo, void *context) {
+    if (sig == SIGUSR1) {
+        ++actual_signal_counter;
+    } else {
+        printf("Expected counter: %d, Actual counter: %d\n", expected_signal_counter, actual_signal_counter);
+        return;
+    }
+}
---
+++ cw04/zad3a/utils.c
@@ -0,0 +1,37 @@
+#include "utils.h"
+
+
+void initialize_signals(void(*fun)(int, siginfo_t *, void *)) {
+    sigset_t signals;
+
+    if (sigfillset(&signals) == -1) {
+        printf("PROBLEM WITH SIGFILLSET METHOD\n");
+        return;
+    }
+
+    sigdelset(&signals, SIGUSR1);
+    sigdelset(&signals, SIGUSR2);
+
+    if (sigprocmask(SIG_BLOCK, &signals, NULL) < 0) {
+        printf("PROBLEM WITH SIGPROCMASK METHOD\n");
+        return;
+    }
+
+    sigset_t signals2;
+    if (sigemptyset(&signals2) < 0) {
+        printf("PROBLEM WITH SIGEMPTYSET METHOD\n");
+        return;
+    }
+
+    struct sigaction sigaction1;
+    sigaction1.sa_sigaction = fun;
+    sigaction1.sa_mask = signals2;
+    sigaction1.sa_flags = SA_SIGINFO;
+
+    if (sigaction(SIGUSR1, &sigaction1, NULL) == -1
+        || sigaction(SIGUSR2, &sigaction1, NULL) == -1) {
+
+        printf("PROBLEM WITH SIGACTION METHOD\n");
+        return;
+    }
+}
