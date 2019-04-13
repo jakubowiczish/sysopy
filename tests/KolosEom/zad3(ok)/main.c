a2ba4a1057c24ad3a84e5298f17e36d059733c4f
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-13 21:55:24 +0200

    Add solution for KolosEom/zad3

---
+++ tests/KolosEom/zad3(ok)/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int pdesk[2];
+    if (pipe(pdesk) == -1) {
+        perror("PIPE");
+        return 1;
+    }
+
+    switch (fork()) {
+        case -1:
+            perror("FORK");
+            return 2;
+        case 0:
+            dup2(pdesk[1], STDOUT_FILENO);
+
+//            // ??
+//            close(pdesk[0]);
+//            close(pdesk[1]);
+
+            execvp("ls", argv);
+            perror("EXECVP ls");
+            exit(1);
+        default: {
+            /* Zamknij deskryptor do zapisu,
+             * skopiuj deskyptor końca do odczytu tego potoku na deskryptor wejścia standardowego,
+             * wykonaj  tr "a-z" "A-Z", w przypadku błędu  obsłuż go i wyjdź, zwracając 3.
+            */
+            close(pdesk[1]);
+            dup2(pdesk[0], STDIN_FILENO);
+            if (execlp("tr", "tr", "\"a-z\"", "\"A-Z\"", (char *) NULL) == -1) {
+                perror("EXECLP tr");
+                return 3;
+            }
+
+            /* koniec */
+        }
+    }
+    return 0;
+}
