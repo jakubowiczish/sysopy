3db9a54b5774effa9c34863826f07eed875a062b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-06 01:34:46 +0800

    Delete cw10 directory

--- cw10/another_solution/zad1/proto.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROTO_H
-#define PROTO_H
-
-#include <stdint.h>
-
-void proto_send(int fd, const char* buffer, int32_t size);
-char* proto_recv(int fd);
-
-#endif
--- cw10/complete_sollution/zad1/utils.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int parse_int(const char* string, int* out);
-int parse_double(const char* string, double* out);
